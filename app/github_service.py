7fdbdc3fed1813e4011263b2bb178254f45873d9
saimca-75 <141533236+saimca-75@users.noreply.github.com>
2026-03-27 17:28:01 +0530

    Raise HTTPException on API request errors

--- app/github_service.py
+++ app/github_service.py
@@ -1,4 +1,5 @@
 import requests
+from fastapi import HTTPException
 from app.config import GITHUB_TOKEN, BASE_URL
 
 headers = {
@@ -11,13 +12,16 @@ def get_my_repositories(visibility):
     visibility = visibility.lower()
     url = f"{BASE_URL}/user/repos"
     response = requests.get(url, headers=headers)
-
+    
     if response.status_code != 200:
-        return {"error": response.json()}
+        raise HTTPException(
+        status_code=response.status_code,
+        detail=response.json()
+                              )
 
     repos = response.json()
 
-        #  Apply filtering
+        # Apply filtering
     if visibility == "public":
         repos = [r for r in repos if not r["private"]]
     elif visibility == "private":
@@ -39,8 +43,10 @@ def get_public_repositories(username):
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
-        return {"error": response.json()}
-
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json()
+        )
     return response.json()
 
 
@@ -56,8 +62,10 @@ def create_issue(owner, repo, title, body):
     response = requests.post(url, json=data, headers=headers)
 
     if response.status_code != 201:
-        return {"error": response.json()}
-
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json()
+        )
     return response.json()
 
 # 4. list issues
@@ -66,7 +74,10 @@ def list_issues(owner, repo):
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
-        return {"error": response.json()}
+        raise HTTPException(
+              status_code=response.status_code,
+              detail=response.json()
+                       )
 
     issues = response.json()
 
@@ -78,3 +89,48 @@ def list_issues(owner, repo):
         }
         for issue in issues
     ]
+
+
+# 5. Pull Commits
+def get_commits(owner, repo):
+    url = f"{BASE_URL}/repos/{owner}/{repo}/commits"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json()
+        )
+
+    commits = response.json()
+
+    return [
+        {
+            "message": commit["commit"]["message"],
+            "author": commit["commit"]["author"]["name"],
+            "url": commit["html_url"]
+        }
+        for commit in commits
+    ]
+
+
+# 5. create a Pull request
+def create_pull_request(owner, repo, title, body, head, base):
+    url = f"{BASE_URL}/repos/{owner}/{repo}/pulls"
+
+    data = {
+        "title": title,
+        "body": body,
+        "head": head,   # your branch
+        "base": base    # target branch (usually main)
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    if response.status_code != 201:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json()
+        )
+
+    return response.json()
