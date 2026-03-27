65bfea5f8fab482a1cda7677e6d0f59844be5c66
saimca-75 <141533236+saimca-75@users.noreply.github.com>
2026-03-27 10:22:25 +0530

    Update comments for clarity in routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,22 +8,22 @@ from app.github_service import (
 
 router = APIRouter()
 
-# 🔹 Public repos
+# Public repos
 @router.get("/repos/{username}")
 def fetch_public_repos(username: str):
     return get_public_repositories(username)
 
-# 🔹 Private + Public (authenticated)
+# Private + Public (authenticated)
 @router.get("/my-repos")
 def fetch_my_repos(visibility: str = "all"):
     return get_my_repositories(visibility)
 
-# 🔹 Create issue
+# Create issue
 @router.post("/create-issue")
 def create_issue_api(owner: str, repo: str, title: str, body: str):
     return create_issue(owner, repo, title, body)
 
-# 🔹 list issues
+# list issues
 @router.get("/list-issues")
 def list_issues_api(owner: str, repo: str):
     return list_issues(owner, repo)
